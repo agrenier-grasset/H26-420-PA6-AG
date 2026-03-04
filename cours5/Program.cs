5a2a75e5e9b7f275d161347c08dece7f05589a66
Antoine Grenier <agrenier@cegepgranby.qc.ca>
2026-03-04 15:50:52 -0500

    Ajout début cours 5

---
+++ cours5/Program.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.Data.SqlClient;
+
+string connectionString = "Server=(localdb)\\mssqllocaldb;Database=MaBase;" +
+    "Trusted_Connection=True;MultipleActiveResultSets=true";
+
+try
+{
+    using (SqlConnection sqlConnection = new SqlConnection(connectionString))
+    {
+        sqlConnection.Open();
+        Console.WriteLine("Connexion ouverte avec succès.");
+    }
+}
+catch (SqlException ex)
+{
+    Console.WriteLine($"Erreur SQL : {ex.Message}");
+}
+catch (Exception ex)
+{
+    Console.WriteLine($"Erreur générale : {ex.Message}");
+}
